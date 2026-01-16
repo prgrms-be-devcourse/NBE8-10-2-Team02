cacf8230649cd8dae4d09f7562ade5dba7bedcfa
MADUPAL <kevin488@naver.com>
2026-01-16 15:46:51 +0900

    Feat :#4 Entity 추가-누락된거추가

--- src/main/java/com/back/domain/post/postComment/entity/PostComment.java
+++ src/main/java/com/back/domain/post/postComment/entity/PostComment.java
@@ -3,7 +3,10 @@ package com.back.domain.post.postComment.entity;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.post.post.entity.Post;
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -11,6 +14,9 @@ import java.time.LocalDateTime;
 
 import static jakarta.persistence.FetchType.*;
 
+@Entity
+@Getter
+@NoArgsConstructor
 public class PostComment extends BaseEntity {
 
     private String content;
--- src/main/java/com/back/domain/tag/tag/entity/Tag.java
+++ src/main/java/com/back/domain/tag/tag/entity/Tag.java
@@ -1,7 +1,13 @@
 package com.back.domain.tag.tag.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor
 public class Tag extends BaseEntity {
 
     private String content;
