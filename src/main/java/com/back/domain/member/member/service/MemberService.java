f31d9347af5d89c7653090356a179ee3358458a7
kimdohwan7220 <dohwan8220@naver.com>
2026-01-19 16:37:05 +0900

    feat: #15 MemberService 이메일 조회 추가

---
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.back.domain.member.member.service;
+
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.repository.MemberRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Optional<Member> findByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+}
