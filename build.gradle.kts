96ff6fd57e955082d3ca9dd0c0308e9063d6c88a
Jiny <lozy960422@gmail.com>
2026-01-19 09:21:22 +0900

    [feat] jdbc와 스프링 시큐리티 import 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-session-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 
@@ -40,6 +41,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-session-jdbc-test")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 
@@ -47,8 +49,8 @@ dependencies {
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-//    implementation("org.springframework.boot:spring-boot-starter-security")
-//    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 }
 
 tasks.withType<Test> {
