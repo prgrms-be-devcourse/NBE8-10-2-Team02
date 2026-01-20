ca4eba4e19c8046cc6d0b60a6c30d87955fba797
sia <piao516@naver.com>
2026-01-20 12:03:47 +0900

    repository 파일  추가

--- src/main/java/com/back/domain/game/game/dto/GameSearchCondition.java
+++ src/main/java/com/back/domain/game/game/dto/GameSearchCondition.java
@@ -1,4 +1,14 @@
 package com.back.domain.game.game.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GameSearchCondition {
+
+    private List<Long> genreIds;
+
+    private List<Long> platformIds;
+
 }
