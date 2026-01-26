7f1688f1bc8ca4e36016b206dc0c70593251da9b
kimdohwan7220 <dohwan8220@naver.com>
2026-01-26 18:44:02 +0900

    refactor: #51 gradle 다시 원상 복구

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.0")
 
