3e756c6b3d8603ce9d9cf342542357b3ae6a21b5
Jiny <lozy9604@naver.com>
2026-01-27 14:31:46 +0900

    chore: 안 쓰는 메서드 삭제

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.game.game.service.GameService;
 import com.back.domain.member.member.service.MemberService;
 import com.back.global.app.AppConfig;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
 
     private final MemberService memberService;
+    private final GameService gameService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
@@ -26,6 +30,20 @@ public class BaseInitData {
         tryJoin("admin@test.com", "1234", "관리자");
         tryJoin("user1@test.com", "1234", "유저1");
         tryJoin("user2@test.com", "1234", "유저2");
+        try {
+            gameService.createGame(123L,
+                    "Cat Mario",
+                    "고양이 마리오 게임",
+                    "cat_mario.png",
+                    LocalDate.now());
+            gameService.createGame(124L,
+                    "Super Mario Bros.",
+                    "최초의 마리오 게임",
+                    "super_mario_bros.png",
+                    LocalDate.of(1985, 9,13));
+        } catch (Exception ignored) {
+        }
+
     }
 
     private void tryJoin(String email, String password, String nickname) {
