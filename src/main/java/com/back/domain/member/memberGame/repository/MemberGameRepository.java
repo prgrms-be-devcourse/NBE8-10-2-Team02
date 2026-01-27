dc24069afc583851a6fe27d6d226ac0e3f5f4c80
Jiny <lozy9604@naver.com>
2026-01-27 14:28:55 +0900

    feat: Membergame에 삭제/업데이트 기능추가. MemberGame에 컬럼 추가

--- src/main/java/com/back/domain/member/memberGame/repository/MemberGameRepository.java
+++ src/main/java/com/back/domain/member/memberGame/repository/MemberGameRepository.java
@@ -3,5 +3,8 @@ package com.back.domain.member.memberGame.repository;
 import com.back.domain.member.memberGame.entity.MemberGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberGameRepository extends JpaRepository<MemberGame, Integer> {
+    Optional<MemberGame> findByMemberIdAndGameId(int memberId, int gameId);
 }
