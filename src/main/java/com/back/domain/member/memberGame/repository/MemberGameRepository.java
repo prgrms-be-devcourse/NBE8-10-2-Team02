b6b388b0d7fc6d6bce6c4fc1406d7be3185cf0ca
Jiny <lozy9604@naver.com>
2026-01-28 16:18:51 +0900

    feat:library 조회에 필터링 추가

--- src/main/java/com/back/domain/member/memberGame/repository/MemberGameRepository.java
+++ src/main/java/com/back/domain/member/memberGame/repository/MemberGameRepository.java
@@ -1,5 +1,6 @@
 package com.back.domain.member.memberGame.repository;
 
+import com.back.domain.member.memberGame.StatusEnum;
 import com.back.domain.member.memberGame.entity.MemberGame;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,4 +12,10 @@ public interface MemberGameRepository extends JpaRepository<MemberGame, Integer>
     Optional<MemberGame> findByMemberIdAndGameId(int memberId, int gameId);
 
     Page<MemberGame> findByMemberId(int memberId, Pageable pageable);
+
+    Page<MemberGame> findByMemberIdAndStatus(int memberId, StatusEnum status, Pageable pageable);
+
+    Page<MemberGame> findByMemberIdAndPlatform(int memberId, String platform, Pageable pageable);
+
+    Page<MemberGame> findByMemberIdAndStatusAndPlatform(int memberId, StatusEnum status, String platform, Pageable pageable);
 }
